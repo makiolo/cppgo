7e1ec1ced6eabfa1728dffc5a534b0a8c6e2b301
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-12-08 22:38:12 +0100

    fix merge

---
+++ tests/test_01.cpp
@@ -0,0 +1,15 @@
+
+char const* greet()
+{
+   return "hello, world";
+}
+
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(hello)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
+
+
