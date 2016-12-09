09ec275b8936e2ae9ca492cb2d3841f07c768fb3
Ricardo <makiolo@gmail.com>
2016-12-09 17:01:48 +0100

    Update test_01.cpp

--- tests/test_01.cpp
+++ tests/test_01.cpp
@@ -1,15 +1,12 @@
+#include <boost/python.hpp>
 
 char const* greet()
 {
-   return "hello, world";
+   return "hello cppgo";
 }
-
-#include <boost/python.hpp>
 
 BOOST_PYTHON_MODULE(hello)
 {
     using namespace boost::python;
     def("greet", greet);
 }
-
-
