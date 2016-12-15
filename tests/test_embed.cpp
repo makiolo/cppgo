deea82836f633bee92d172803fc14c3e33e673f6
Ricardo <makiolo@gmail.com>
2016-12-15 12:15:33 +0100

    Update test_embed.cpp

--- tests/test_embed.cpp
+++ tests/test_embed.cpp
@@ -41,8 +41,19 @@ TEST(PythonTest, Test1)
 		py::object mainmodule = py::import("__main__");
 		py::object globals = mainmodule.attr("__dict__");
 		py::object result = py::exec_file("hello.py", globals, globals);
-		py::dict output = py::extract<py::dict>(globals);
-		output["func1"](1, 2, 3, 5.0f, "hiiii from c++");
+		py::dict module_dict = py::extract<py::dict>(globals);
+		
+		// call static method
+		module_dict["func1"](1, 2, 3, 5.0f, "hiiii from c++");
+		
+		// instance class
+		py::object class_derived = module_dict["PythonDerived"];
+		py::object instance_derived = class_derived("from c++", 9876);
+		// 
+		
+		// 
+		// std::shared_ptr<Base> a = asla("hello.PythonDerived");
+		// a->ddd();
 	}
 	catch (const py::error_already_set& /*e*/)
 	{
