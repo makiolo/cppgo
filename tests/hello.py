9c2bfbe04a0b53381ea213f2690fa91cd7a95ea1
Ricardo <makiolo@gmail.com>
2016-12-16 15:47:25 +0100

    Update hello.py

--- tests/hello.py
+++ tests/hello.py
@@ -37,18 +37,25 @@ inheritance.fb(x)
 
 import factory
 
-class PythonDerived(factory.Base):
+class PythonDerived1(factory.Base):
     def name(self):
-        return "base: {} and derived: {}".format(super(PythonDerived, self).name(), "PythonDerived")
+        return "PythonDerived1 -> base: {} and derived: {}".format(super(PythonDerived1, self).name(), "PythonDerived1")
 
+class PythonDerived2(factory.Base):
+    def name(self):
+        return "PythonDerived2 -> base: {} and derived: {}".format(super(PythonDerived2, self).name(), "PythonDerived2")
+
+factory.register("PythonDerived1")
+factory.register("PythonDerived2")
+
+print ("creating A since python")
 f1 = factory.create("A", "hi", 1234)
+print ("creating B since python")
 f2 = factory.create("B", "hizzz", 1234567)
-print (f1)
+print ("show A name")
 print (f1.name())
-print (f2)
+print ("show B name")
 print (f2.name())
-
-factory.register("hello.PythonDerived")
 
 commands = {}
 def load_simple_tasks(plugin_folder, main_module = '__init__'):
