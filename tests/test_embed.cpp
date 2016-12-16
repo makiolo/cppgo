8fd6688e9b5a79f9f798764f9070d36273b383b8
Ricardo <makiolo@gmail.com>
2016-12-16 12:06:21 +0100

    Update test_embed.cpp

--- tests/test_embed.cpp
+++ tests/test_embed.cpp
@@ -4,6 +4,7 @@
 #include <teelogging/teelogging.h>
 #include <boost/python.hpp>
 #include <gtest/gtest.h>
+#include <dp14/factory.h>
 
 using namespace boost::python;
 namespace py = boost::python;
@@ -36,25 +37,95 @@ struct PythonEmbed
 	wchar_t *program;
 };
 
+class BaseExample
+{
+public:
+	using factory = dp14::factory<BaseExample, std::string, int>;
+
+	explicit BaseExample(std::string name, int q)
+		: _name(name)
+		, _q(q)
+	{
+		std::cout << "constructor " << _name << " - " << _q << std::endl;
+	}
+	virtual ~BaseExample() { std::cout << "destruction" << std::endl; }
+	virtual std::string name() const { return "Base"; }
+
+protected:
+	std::string _name;
+	int _q;
+};
+
+class A_Example : public BaseExample
+{
+public:
+	DEFINE_KEY(A_Example)
+	explicit A_Example(std::string name, int q) : BaseExample(name, q) { ; }
+	virtual ~A_Example() = default;
+	virtual std::string name() const { return "A_Example"; }
+};
+
+class B_Example : public BaseExample
+{
+public:
+	DEFINE_KEY(B_Example)
+	explicit B_Example(std::string name, int q) : BaseExample(name, q) { ; }
+	virtual ~B_Example() = default;
+	virtual std::string name() const { return "B_Example"; }
+};
+
+class PythonDerived : public BaseExample
+{
+public:
+	DEFINE_KEY(PythonDerived)
+	explicit PythonDerived(std::string name, int q) : BaseExample(name, q)
+	{ 
+		// init
+		py::object mainmodule = py::import("__main__");
+		py::object globals = mainmodule.attr("__dict__");
+		py::exec_file("hello.py", globals, globals);
+		// instance
+		py::object class_derived = globals["PythonDerived"];
+		_instance = class_derived(name, q);
+		/*
+		// call static method
+		LOGI("call func1() in hello.py");
+		globals["func1"](1, 2, 3, 5.0f, "hiiii from c++");
+		*/
+	}
+	virtual ~PythonDerived() = default;
+	virtual std::string name() const
+	{
+		auto result = _instance.attr("name")();
+		return py::extract<std::string>(py::str(result))();
+	}
+protected:
+	py::object _instance;
+};
+
+namespace regA {
+	BaseExample::factory::registrator<A_Example> reg;
+}
+namespace regB {
+	BaseExample::factory::registrator<B_Example> reg;
+}
+namespace regPD {
+	BaseExample::factory::registrator<PythonDerived> reg;
+}
+
 TEST(PythonTest, Test1)
 {
 	PythonEmbed python;
 	try
-	{
-		py::object mainmodule = py::import("__main__");
-		py::object globals = mainmodule.attr("__dict__");
-		py::exec_file("hello.py", globals, globals);
-
-		// instance class and call method
-		LOGI("call PythonDerived.name() class in hello.py");
-		py::object class_derived = globals["PythonDerived"];
-		py::object instance_derived = class_derived("from c++", 9876);
-		py::object result = instance_derived.attr("name")();
-		std::cout << "PythonDerived.name() = " << py::extract<std::string>(py::str(result))() << std::endl;
-
-		// call static method
-		LOGI("call func1() in hello.py");
-		globals["func1"](1, 2, 3, 5.0f, "hiiii from c++");
+	{		
+		auto base1 = Base::factory::instance().create("PythonDerived", "from factory c++", 1234567);
+		std::cout << "base1 = " << base1->name() << std::endl;
+		
+		auto base2 = Base::factory::instance().create("A_Example", "from factory c++", 1234567);
+		std::cout << "base2 = " << base2->name() << std::endl;
+		
+		auto base3 = Base::factory::instance().create("B_Example", "from factory c++", 1234567);
+		std::cout << "base3 = " << base3->name() << std::endl;
 	}
 	catch (const py::error_already_set& /*e*/)
 	{
@@ -62,4 +133,3 @@ TEST(PythonTest, Test1)
 		PyErr_Print();
 	}
 }
-
