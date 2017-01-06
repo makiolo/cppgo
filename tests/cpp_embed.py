c1076ec798be86f9994c2e86bbced38998bc2d58
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2017-01-06 09:10:49 +0100

    improving python factory

---
+++ tests/cpp_embed.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# import os
+# import logging
+# import imp
+
+# import hello
+# print (hello.greet())
+#
+# import classes
+# t = classes.World()
+# t.set("bom dia!")
+# print (t.greet())
+#
+# import ctor
+# c1 = ctor.Ctor("Bonjour")
+# print (c1.greet())
+# c2 = ctor.Ctor(3.141592654, 1.0)
+# print (c2.greet())
+#
+# import member
+# m1 = member.SomeClass("Pavel")
+# print ("name = {}".format(m1.name))
+# m1.name = "Gunther"
+# print ("name = {}".format(m1.name))
+# m1.number = 7.3
+# print ("number = {}".format(m1.number))
+#
+# import inheritance
+# b = inheritance.Base()
+# d = inheritance.Derived()
+# inheritance.fb(b)
+# inheritance.fb(d)
+# inheritance.fd(d)
+# x = inheritance.factory()
+# inheritance.fb(x)
+
+# commands = {}
+# def load_simple_tasks(plugin_folder, main_module = '__init__'):
+#     def load_plugin(plugin):
+#         return imp.load_module(main_module, *plugin["info"])
+#     if os.path.isdir(plugin_folder):
+#         # create plugin list
+#         possibleplugins = os.listdir(plugin_folder)
+#         plugins = []
+#         for i in possibleplugins:
+#             location = os.path.join(plugin_folder, i)
+#             if not os.path.isdir(location) or not main_module + ".py" in os.listdir(location):
+#                 continue
+#             info = imp.find_module(main_module, [location])
+#             plugins.append({"name": i, "info": info})
+#         # load each plugin
+#         for p in plugins:
+#             name = p["name"]
+#             logging.debug("Loading plugin " + name)
+#             plugin = load_plugin(p)
+#             if name not in commands:
+#                 try:
+#                     plugin_class = getattr(plugin, name)
+#                     if issubclass(plugin_class, TaskSimple):
+#                         commands[name] = plugin_class
+#                     else:
+#                         logging.info('Error in plugin %s. Your class "%s" don\'t comply with "TaskSimple" interface.' % (name, name))
+#                 except AttributeError:
+#                     logging.info('Error in plugin %s. Don\'t found a class with name "%s".' % (name, name))
+#             else:
+#                 logging.error('A command with name %s is already used. Error registering plugin.' % name)
+#     else:
+#         logging.debug('No plugins folder found in %s.' % os.path.abspath(plugin_folder))
+#
+# def func1(n1, n2, n3, f1, str1):
+#     for fl in os.listdir('.'):
+#         print (fl)
+#         if (fl == 'teelogging.txt'):
+#             print ("log .......")
+#             with open('teelogging.txt', 'rt') as f:
+#                 content = f.read()
+#             print(content)
+
+import foo_python
+foo_python.init()
+
+f1 = foo_python.create("A", "AAAA", 1234)
+print ("A.name() = %s" % f1.name())
+f2 = foo_python.create("B", "BBBB", 1234567)
+print ("B.name() = %s" % f2.name())
+f3 = foo_python.create("PythonDerived1", "PPPPP", 1234567890)
+print ("P.name() = %s" % f3.name())
+# f4 = foo_python.create("PythonDerived2", "QQQQQ", 1234567891)
+# print ("Q.name() = %s" % f4.name())
+
---
+++ tests/python_embed.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <assert.h>
+#include <tuple>
+#include <teelogging/teelogging.h>
+#include <boost/python.hpp>
+#include <gtest/gtest.h>
+#include <dp14/factory.h>
+#include "foo.h"
+#include "foo_python.h"
+
+using namespace boost::python;
+namespace py = boost::python;
+
+class PythonTest : testing::Test { };
+
+// https://wiki.python.org/moin/boost.python/EmbeddingPython
+struct PythonEmbed
+{
+	PythonEmbed()
+	{
+		LOGI("creating python ...");
+		program = Py_DecodeLocale("python_embed", NULL);
+		if (program == NULL) {
+			fprintf(stderr, "Fatal error: cannot decode\n");
+			exit(1);
+		}
+		Py_SetProgramName(program);
+		Py_Initialize();
+	}
+
+	~PythonEmbed()
+	{
+		LOGI("destroying python ...");
+		Py_Finalize();
+		PyMem_RawFree(program);
+	}
+
+	wchar_t *program;
+};
+
+std::ostream& operator<<(std::ostream& os, const py::str& o)
+{
+    return os << py::extract<std::string>(py::str(o))();
+}
+
+TEST(PythonTest, Test1)
+{
+	// reg c++ implementations
+	init_factory();
+	load_library fooB("foo_python");
+
+	PythonEmbed python;
+	try
+	{
+		auto base1 = foo::Base::get_factory().create("A", "create c++ from factory c++", 111);
+		std::cout << "base1 = " << base1->getKEY() << " end" << std::endl;
+
+		auto base2 = foo::Base::get_factory().create("B", "create c++ from factory c++", 222);
+		std::cout << "base2 = " << base2->getKEY() << " end" << std::endl;
+
+		std::cout << "------------------------" << std::endl;
+		auto base3 = foo::Base::get_factory().create("PythonDerived1", "create python from factory c++", 333);
+		std::cout << "base3 = " << base3->getKEY() << " end" <<  std::endl;
+		std::cout << "------------------------" << std::endl;
+
+		// auto base4 = foo::Base::get_factory().create("PythonDerived2", "create python from factory c++", 999);
+		// std::cout << "base4 = " << base4->getKEY() << " end" <<  std::endl;
+	}
+	catch (const py::error_already_set& /*e*/)
+	{
+		LOGE("exception in boost::python ...");
+		PyErr_Print();
+	}
+}
+
+/*
+LOGI("call func1() in XX.py");
+globals["func1"](1, 2, 3, 5.0f, "hiiii from c++");
+*/
+
