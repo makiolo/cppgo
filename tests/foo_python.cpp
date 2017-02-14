4e4410a73b45f4a2e4ce134c838d96dabe5784e2
Ricardo <makiolo@gmail.com>
2017-02-14 16:41:25 +0100

    Update foo_python.cpp

--- tests/foo_python.cpp
+++ tests/foo_python.cpp
@@ -19,8 +19,51 @@ object init_impl(tuple args, dict kwargs)
 	return object();
 }
 
-object create(tuple args, dict kwargs)
+template<typename ... Args>
+class get_type
 {
+    template <std::size_t N>
+    using type = typename std::tuple_element<N, std::tuple<Args...> >::type;
+};
+
+template <class F, size_t... Is>
+constexpr auto index_apply_impl(F&& f, std::index_sequence<Is...>) {
+    return f(std::integral_constant<size_t, Is> {}...);
+}
+
+template <size_t N, class F>
+constexpr auto index_apply(F&& f) {
+    return index_apply_impl(std::forward<F>(f), std::make_index_sequence<N>{});
+}
+
+template <class Args...>
+constexpr auto expand(const py::tuple& t) {
+    return index_apply<sizeof...(Args)>(
+        [&](auto... Is) {
+        	return make_tuple(
+			py::extract< get_type<Args...>::type<Is> >(t[Is])()...
+		);
+        });
+}
+
+template <typename Function, typename Tuple>
+constexpr auto apply(Function&& f, Tuple&& t) {
+    return index_apply<std::tuple_size<Tuple>{}>(
+        [&](auto... Is) {
+            return std::forward<Function>(f)( std::get<Is>(std::forward<Tuple>(t))... );
+        });
+}
+
+object create(py::tuple args, py::dict kwargs)
+{
+	//auto new_args = expand<std::string, std::string, int>( args );
+	std::cout << "--------------------" << std::endl;
+	std::cout << std::string( py::extract<const char*>(py::str(args[0]))() ) << std::endl;
+	std::cout << py::extract<std::string>(py::str(args[0]))() << std::endl;
+	std::cout << std::string( py::extract<const char*>(args[0])() ) << std::endl;
+	std::cout << py::extract<std::string>(args[0])() << std::endl;
+	std::cout << "--------------------" << std::endl;
+	
 	// for(int i = 0; i < len(args); ++i)
 	// {
 	// 	std::cout << "arg " << i << ": " << py::str(args[i]) << std::endl;
