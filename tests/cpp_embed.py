bdfc289ec253dfff9f542181929dd68e9956dbc5
Ricardo <makiolo@gmail.com>
2017-03-21 16:19:54 +0100

    Update cpp_embed.py

--- tests/cpp_embed.py
+++ tests/cpp_embed.py
@@ -7,19 +7,18 @@ info('foo_python.init()')
 foo_python.init()
 
 info('foo_python.create("A, ....")')
-f1 = foo_python.create("A", "create c++ class from python", 1)
+f1 = foo_python.create2("A", "create c++ class from python", 1)
 
 info('foo_python.create("B, ....")')
-f2 = foo_python.create("B", "create c++ class from python", 2)
+f2 = foo_python.create2("B", "create c++ class from python", 2)
 
 info('foo_python.create("PythonDerived1, ....")')
-f3 = foo_python.create("Python", "PythonDerived1", 3)
+f3 = foo_python.create2("Python", "PythonDerived1", 3)
 
 info('foo_python.create("PythonDerived2, ....")')
-f4 = foo_python.create("Python", "PythonDerived2", 4)
+f4 = foo_python.create2("Python", "PythonDerived2", 4)
 
 info("1.name() = %s" % f1.name())
 info("2.name() = %s" % f2.name())
 info("3.name() = %s" % f3.name())
 info("4.name() = %s" % f4.name())
-
